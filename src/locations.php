1b8a90ae19f16196ce44369dcdb75756ee08e27f
Jason McClurg <mcclujdd@gmail.com>
2023-12-13 16:33:14 -0500

    loc update

--- src/locations.php
+++ src/locations.php
@@ -2,10 +2,10 @@
 
 //assoc array location => address
 $locations = [
-    'Lawrence'=>'1 General St, Lawrence, MA',
-    'Anna Jaques'=>'25 Highland Ave, Newburyport, MA',
-    'Lowell General'=>'275 Varnum Ave, Lowell, MA',
-    'Auburn Office'=>'',
-    'Lowell Saints'=>'',
-    'Marlborough Hospital'=>'159 Union St, Marlborough, MA',
+    'LW'=>'',
+    'AJ'=>'',
+    'LL'=>'',
+    'AB'=>'',
+    'LS'=>'',
+    'MB'=>'',
 ];
