218ddbd8d42301e536e42df4f72fdcabc582d935
jason <mcclujdd@gmail.com>
2024-03-06 16:40:21 -0500

    db change to pdo

--- src/_db.php
+++ src/_db.php
@@ -1,11 +1,17 @@
 <?php
 
-$host = "10.5.0.3";
-$user = "root";
-$pass = $_ENV['MYSQL_ROOT_PASSWORD']; 
-$db = $_ENV['MYSQL_DATABASE'];
-$conn = mysqli_connect($host, $user, $pass, $db,);
-
+//$conn = mysqli_connect($host, $user, $pass, $db,);
 //if (!$conn = mysqli_connect($host, $user, $pass, $db)) {
     //die("Bad connect $host");
 //}
+
+$pdo;
+$host = "10.5.0.3";
+$db = $_ENV['MYSQL_DATABASE'];
+$dsn = "mysql:host=$host;port=3306;dbname=$db";
+$user = "root";
+$pass = $_ENV['MYSQL_ROOT_PASSWORD']; 
+
+if (!$conn = new PDO($dsn, $user, $pass)){
+    die("Bad connect $host");
+}
