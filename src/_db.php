a119b60b7c3b94a847deb6b3bb50ba26699406cf
jason <mcclujdd@gmail.com>
2024-05-17 18:54:30 -0400

    pdo update

--- src/_db.php
+++ src/_db.php
@@ -1,12 +1,10 @@
 <?php
 
-$pdo;
 $host = "10.5.0.3";
 $db = $_ENV['MYSQL_DATABASE'];
-$dsn = "mysql:host=$host;port=3306;dbname=$db";
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;port=3306;dbname=$db;charset=$charset";
 $user = "root";
 $pass = $_ENV['MYSQL_ROOT_PASSWORD']; 
 
-if (!$conn = new PDO($dsn, $user, $pass)){
-    die("Bad connect $host");
-}
+$pdo = new PDO($dsn, $user, $pass);
