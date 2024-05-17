d5fc5e1f9ab5ff802730b25af7f491289f87fa16
jason <mcclujdd@gmail.com>
2024-05-17 09:19:19 -0400

    Trip class

---
+++ src/Trip.php
@@ -0,0 +1,40 @@
+<?php 
+
+Class Trip {
+    private $start_account = '';
+    private $end_account = '';
+    private $message = '';
+    private $valid = '';
+    public $locations = ['Lawrence', 'Anna Jaques', 'Auburn', 'Lowell', 'Home Base',];
+
+}
+    
+
+$start_account = '';
+$end_account = '';
+$message = '';
+$valid = '';
+$locations = ['Lawrence', 'Anna Jaques', 'Auburn', 'Lowell', 'Home Base',];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $start_account = $_POST['start_account'] ?? ''; 
+    $end_account = $_POST['end_account'] ?? ''; 
+    $valid = in_array($start_account, $locations) & in_array($end_account, $locations);
+    $message = $valid ? "Thank you" : "Please Select Start and End Locations";
+}
+?>
+
+<?= $message ?>
+<form action='Trip.php' method='post'>
+    </br><span>Starting Location: </span>
+    <?php foreach ($locations as $opt1) {?>
+        <?= $opt1 ?><input type=radio name='start_account' value='<?= $opt1 ?>' <?= ($start_account == $opt1) ? 'checked' : '' ?>>
+    <?php }?>
+
+    </br><span>Ending Location: </span>
+    <?php foreach ($locations as $opt2) {?>
+        <?= $opt2 ?><input type=radio name='end_account' value='<?= $opt2 ?>' <?= ($end_account == $opt2) ? 'checked' : '' ?>>
+    <?php }?>
+    </br>
+    <input type='submit' value='Save'>
+</form>
