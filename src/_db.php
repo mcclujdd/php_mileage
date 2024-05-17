accf42476402b73358f1d09e1ec0afe3d05c35cb
jason <mcclujdd@gmail.com>
2024-05-17 19:13:53 -0400

    pdo options

--- src/_db.php
+++ src/_db.php
@@ -6,5 +6,10 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;port=3306;dbname=$db;charset=$charset";
 $user = "root";
 $pass = $_ENV['MYSQL_ROOT_PASSWORD']; 
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
 
 $pdo = new PDO($dsn, $user, $pass);
