447835b57779c48443954357107c84f3a413d1a7
jason <mcclujdd@gmail.com>
2024-07-30 02:37:36 -0400

    added form

--- src/index.php
+++ src/index.php
@@ -4,8 +4,8 @@ require_once 'vendor/autoload.php';
 $temporal = new Mileage\Temporal();
 $today = $temporal->get_today();
 
-include_once './locations.php';
 include_once './_db.php';
+
 ?>
 
 <html>
@@ -16,6 +16,22 @@ include_once './_db.php';
 </head>
     <body>
     <p>Index Page loaded. Today's date is <?= $today ?>.</p>
+    <span>New Entry</span>
+    <form method=post action=index.php>
+        <label>Date</label>
+        <input type=text name="tripDate"></br>
+        <label>Start Odo</label>
+        <input type=text name="startOdo"></br>
+        <label>End Odo</label>
+        <input type=text name="endOdo"></br>
+        <label>Start Loc</label>
+        <input type=text name="startLoc"></br>
+        <label>End Loc</label>
+        <input type=text name="endLoc"></br>
+        <label>Trip Miles</label>
+        <input type=text name="tripMiles"></br>
+        <button type=submit>Submit</button>
+    </form>
     <table>
         <th>date</th>
         <th>starting odometer</th>
