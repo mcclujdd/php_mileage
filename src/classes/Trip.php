d9b6517304de955ce7916a32d746a07948cb754b
jason <mcclujdd@gmail.com>
2024-08-03 19:14:35 -0400

    create trip obj with data

--- src/classes/Trip.php
+++ src/classes/Trip.php
@@ -2,11 +2,20 @@
 namespace Mileage;
 
 Class Trip {
-    private $start_account = '';
-    private $end_account = '';
-    private $message = '';
-    private $valid = '';
+    public $data;
     public $locSql = 'SELECT name FROM location';
+
+    public function __construct(string $date, string $loc1, string $loc2, int $odo1, int $odo2, int $miles = 0){
+	$this->data = [
+	    $date,
+	    $loc1,
+	    $loc2,
+	    $odo1,
+	    $odo2,
+	    $miles
+	];
+    }
+
 
     //add trip function
 
--- src/tests/TripTest.php
+++ src/tests/TripTest.php
@@ -5,7 +5,8 @@ use Mileage\Trip;
 
 class TripTest extends TestCase {
     // test things
-    public function testLocSqlIsStr(): void
+    /*
+     * public function testLocSqlIsStr(): void
     {
         $trip = new Trip();
 
@@ -13,6 +14,29 @@ class TripTest extends TestCase {
 
         $this->assertSame('SELECT name FROM location', $sql);
     }
+     */
+
+    public function testTripDataArrayExists(): void
+    {
+        $data = [
+            "date"=>"2024-01-01",
+            "loc1"=>"Lawrence",
+            "loc2"=>"Lowell",
+            "odo1"=>1000,
+            "odo2"=>1017,
+            "miles"=>17,
+        ];
+        $trip = new Trip(...$data);
+        $this->assertNotEmpty($trip->data);
+    }
+
+    /*
+    public function testTripObjInit(): void
+    {
+        $trip = new Trip();
+        $tripData = Trip()->getData()
+    }
+     */
 }
 
 ?>
